0c7914f46cde81c95464176e23f3dd999545484d
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-03 10:09:03 +0200

    implementing GestureDetector interfaces methods

--- app/src/main/java/com/example/swap_function/MainActivity.java
+++ app/src/main/java/com/example/swap_function/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.swap_function;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.GestureDetector;
+import android.support.v4.view.ScaleGestureDetectorCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener//flinging and scrolling,
+GestureDetector.OnDoubleTapListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
