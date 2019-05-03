b8e5e47133de935d015f6cc1aae3bc83eff6aab1
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-03 12:09:08 +0200

    providing text to each method to know when it runs

--- app/src/main/java/com/example/swap_function/MainActivity.java
+++ app/src/main/java/com/example/swap_function/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.swap_function;
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -18,16 +19,24 @@ GestureDetector.OnDoubleTapListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        zuzasMessage = (TextView)findViewById(R.id.zuzasMessage);//reference to the message
+        this.gestureDetector = new GestureDetector(this, this);
+        gestureDetector.setOnDoubleTapListener(this);
+
+
+
     }
 
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent e) {
+        zuzasMessage.setText("onSingleTapConfirmed");
         return false;
     }
 
     @Override
     public boolean onDoubleTap(MotionEvent e) {
+        zuzasMessage.setText("onDoubleTap");
         return false;
     }
 
@@ -38,31 +47,37 @@ GestureDetector.OnDoubleTapListener{
 
     @Override
     public boolean onDown(MotionEvent e) {
+        zuzasMessage.setText("onDown");
         return false;
     }
 
     @Override
     public void onShowPress(MotionEvent e) {
+        zuzasMessage.setText("onShowPress");
 
     }
 
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
+        zuzasMessage.setText("onSingleTapUp");
         return false;
     }
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        zuzasMessage.setText("onScroll");
         return false;
     }
 
     @Override
     public void onLongPress(MotionEvent e) {
+        zuzasMessage.setText("onLongPress");
 
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        zuzasMessage.setText("onFling");
         return false;
     }
 }
