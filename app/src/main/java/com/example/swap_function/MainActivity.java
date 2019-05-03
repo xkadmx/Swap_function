855b6659816d983b4832d5358e32093a61ef0321
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-03 10:28:45 +0200

    implementing Gesture interfaces methods

--- app/src/main/java/com/example/swap_function/MainActivity.java
+++ app/src/main/java/com/example/swap_function/MainActivity.java
@@ -7,12 +7,62 @@ import android.view.MotionEvent;
 import android.view.GestureDetector;
 import android.support.v4.view.ScaleGestureDetectorCompat;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener//flinging and scrolling,
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,//flinging and scrolling,
 GestureDetector.OnDoubleTapListener{
+
+    private TextView zuzasMessage;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
